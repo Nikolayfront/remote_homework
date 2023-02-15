9b67cd1bf88f3ccd725132e2beeb96a1a651b404
Nikolay Frontend <pavlovnch@gmail.com>
2023-02-15 23:05:25 +0700

    changed js file

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ window.onload = function () {
 
     let inputFullName = document.getElementById('fullName');
     let inputUserName = document.getElementById('userName');
+
+    console.log ('Добавили ветку dev-2');
