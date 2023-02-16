f25b08347e32e7d491bc74b1a4a297ac8e14fab4
Nikolay Frontend <pavlovnch@gmail.com>
2023-02-16 18:47:49 +0700

    Revert "changed config file"
    This reverts commit 666847c9e806090ef844169d4d3884db42991235.

--- config.js
+++ config.js
@@ -4,7 +4,3 @@ window.onload = function () {
     let inputUserName = document.getElementById('userName');
 
     console.log ('Добавили ветку dev-2');
-
-    console.log ('Добавляем строку согласно п.10');
-    console.log ('Добавляем строку согласно п.10');
-
